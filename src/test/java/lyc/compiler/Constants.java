5412725122169b12ff686e54c8aab94865a4078f
Nacho027 <i.jose.lopez.93@gmail.com>
2023-04-24 14:18:31 -0300

    fixing parser & tests

--- src/test/java/lyc/compiler/Constants.java
+++ src/test/java/lyc/compiler/Constants.java
@@ -1,6 +1,6 @@
 package lyc.compiler;
 
 public final class Constants {
-    public static final String EXAMPLES_ROOT_DIRECTORY = "https://raw.githubusercontent.com/Lenguajes-y-Compiladores-UnLaM/consignas-tp/main/ejemplos";
+    public static final String EXAMPLES_ROOT_DIRECTORY = "https://github.com/Lenguajes-y-Compiladores-UnLaM/consignas-tp/blob/main/ejemplos";
 
 }

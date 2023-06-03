3d42a7d06da3f044269be1a6ebb6438a71217607
Tomas Di Vito <tomasdv2@gmail.com>
2023-06-03 17:40:45 -0300

    implemented datatypes update on grammar rules

--- src/main/java/Tercetos/TercetoGenerator.java
+++ src/main/java/Tercetos/TercetoGenerator.java
@@ -5,13 +5,13 @@ import java.util.ArrayList;
 public class TercetoGenerator {
     public static ArrayList<Terceto> tercetos = new ArrayList<Terceto>();
 
-    public Integer crearTerceto(String operador, String primer_operando, String segundo_operando) {
+    public static Integer crearTerceto(String operador, String primer_operando, String segundo_operando) {
         Terceto terceto = new Terceto(operador, primer_operando, segundo_operando);
         tercetos.add(terceto);
         return tercetos.size()-1;
     }
 
-    public Integer crearTerceto(String operando) {
+    public static Integer crearTerceto(String operando) {
         Terceto terceto = new Terceto(operando);
         tercetos.add(terceto);
         return tercetos.size()-1;
